7b3ae39e9a062dbde0b7a88298a22c6ccfd661cb
Jordan Laimana <jlaimana@amazon.com>
2020-10-02 20:21:19 +0000

    Init resource handlers

---
+++ aws-ses-simpleemailresource/src/main/java/software/amazon/ses/simpleemailresource/Configuration.java
@@ -0,0 +1,8 @@
+package software.amazon.ses.simpleemailresource;
+
+class Configuration extends BaseConfiguration {
+
+    public Configuration() {
+        super("aws-ses-simpleemailresource.json");
+    }
+}
